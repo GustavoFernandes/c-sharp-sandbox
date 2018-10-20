4fa75840fb7ca3d13034f4d903cfce96c52e966a
Gustavo A. Fernandes <GustavoFernandes@users.noreply.github.com>
2018-10-19 20:03:28 -0700

    Add initial syntax class

---
+++ UnitTestProject/UnitTestProject/Syntax.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mathematics = System.Math; // using alias directive
+
+using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert; // using static Directive
+
+namespace UnitTestProject
+{
+	[TestClass] // <- attributes
+	public class Syntax // <- test class must be public for test runner to find it
+	{
+		[TestMethod]
+		public void UsingAliasDirectiveExample()
+		{
+			AreEqual(16, Mathematics.Pow(4, 2));
+		}
+	}
+}
