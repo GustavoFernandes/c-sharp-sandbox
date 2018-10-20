8620dd638471fdd86d36fdd91833ed64977a659b
Gustavo A. Fernandes <GustavoFernandes@users.noreply.github.com>
2018-10-19 20:09:58 -0700

    Add initial tests class

---
+++ UnitTestProject/UnitTestProject/Tests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace UnitTestProject
+{
+	[TestClass]
+    public class Tests
+    {
+        #region Short Circuit
+
+        private int shortCircuitTestCounter = 0;
+
+        [TestMethod]
+        public void ShortCircuit()
+        {
+            IncrementShortCircuitCounter();
+            AreEqual(1, shortCircuitTestCounter);
+
+            if (true && IncrementShortCircuitCounter()) { /* no-op */ }
+            AreEqual(2, shortCircuitTestCounter);
+
+            // short-circuits and doesn't increment
+            if (false && IncrementShortCircuitCounter()) { /* no-op */ }
+            AreEqual(2, shortCircuitTestCounter);
+
+            if (IncrementShortCircuitCounter() && false) { /* no-op */ }
+            AreEqual(3, shortCircuitTestCounter);
+        }
+
+        private bool IncrementShortCircuitCounter()
+        {
+            shortCircuitTestCounter++;
+            return true;
+        }
+
+        #endregion
+    }
+}
